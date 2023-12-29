5a131c63efac1e4b8a9e94945b09b990b9ff51cb
Jamol Shoymurzayev <J.Shoymurzayev@newuu.uz>
2023-12-29 22:14:44 +0500

    testing0

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,44 +1,62 @@
+
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <utility>
+#include <climits>
 
 using namespace std;
 
-class Problem1 {
-public:
-    void dijkstra(const vector<vector<pair<int, int>>>& graph, int source) {
-        const int INF = 1e9;
-        int n = graph.size();
-        vector<int> dist(n, INF);
-        vector<int> parent(n, -1);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+typedef pair<int, int> pii;
 
-        dist[source] = 0;
-        pq.push({0, source});
+const int MAX_VERTICES = 4; // Number of vertices in the graph
 
-        while (!pq.empty()) {
-            int u = pq.top().second;
-            pq.pop();
+vector<pii> adj[MAX_VERTICES]; // Adjacency list for the graph
 
-            for (auto& edge : graph[u]) {
-                int v = edge.first;
-                int w = edge.second;
+void dijkstra(int src) {
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    vector<int> dist(MAX_VERTICES, INT_MAX);
+    vector<bool> visited(MAX_VERTICES, false);
 
-                if (dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
-                    parent[v] = u;
-                    pq.push({dist[v], v});
-                }
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (visited[u]) continue;
+        visited[u] = true;
+
+        for (auto &edge : adj[u]) {
+            int v = edge.first;
+            int weight = edge.second;
+
+            if (!visited[v] && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
             }
         }
-
-        // Print the shortest distances
-        cout << "Shortest distances from source " << source << ":\n";
-        for (int i = 0; i < n; i++) {
-            cout << i << ": " << dist[i] << "\n";
-        }
-
-        // Print the shortest paths (optional)
-        // ...
     }
-};
+
+    for (int i = 0; i < MAX_VERTICES; ++i)
+        cout << "Vertex " << i << " Distance from Source " << src << ": " << dist[i] << endl;
+}
+
+int main() {
+    // Graph representation
+    adj[0].push_back(make_pair(1, 100));
+    adj[0].push_back(make_pair(3, 100));
+    adj[1].push_back(make_pair(0, 100));
+    adj[1].push_back(make_pair(2, 100));
+    adj[1].push_back(make_pair(3, 600));
+    adj[2].push_back(make_pair(1, 100));
+    adj[2].push_back(make_pair(3, 200));
+    adj[3].push_back(make_pair(0, 100));
+    adj[3].push_back(make_pair(1, 600));
+    adj[3].push_back(make_pair(2, 200));
+
+    dijkstra(0); // Calling dijkstra for source vertex 0
+
+    return 0;
+}
